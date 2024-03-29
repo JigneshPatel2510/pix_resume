cb629d340a6136649b22fdb0854168a9bbff2bcf
Jignesh Patel <officialjignesh25@gmail.com>
2024-03-29 16:07:17 +0530

    commit  splash animation

--- lib/pdf_preview.dart
+++ lib/pdf_preview.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Preview extends StatelessWidget {
   final String? filePath;
@@ -23,7 +27,33 @@ class Preview extends StatelessWidget {
             )),
         actions: [
           IconButton(
-              onPressed: () {},
+              onPressed: () async {
+                PermissionStatus result = await Permission.storage.request();
+                if (result.isGranted) {
+                  String sourceFilePath = filePath.toString();
+                  Directory generalDownloadDir = Directory(
+                      '/storage/emulated/0/Download'); //! THIS WORKS for android only !!!!!!
+
+                  String qrName = "jigigig";
+
+                  //qr image file saved to general downloads folder
+                  File qrJpg =
+                      await File('${generalDownloadDir.path}/$qrName.pdf')
+                          .create();
+                  try {
+                    // Copy the file
+                    File(sourceFilePath).copySync(qrJpg.path);
+
+                    // File copied successfully
+                    debugPrint('File copied to device storage download folder');
+                  } catch (e) {
+                    // Error copying the file
+                    debugPrint('Error: $e');
+                  }
+                } else {
+                  debugPrint('Permissions denied');
+                }
+              },
               icon: const Icon(
                 Icons.save,
                 color: Colors.black,
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pix_resume/bottomtabbar.dart';
-import 'package:pix_resume/hometab.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -15,7 +15,7 @@ class _SplashState extends State<Splash> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(const Duration(seconds: 3),() {
+    Future.delayed(const Duration(seconds: 5),() {
       Get.to(const BottomTabBar());
 
     },);
@@ -24,14 +24,16 @@ class _SplashState extends State<Splash> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurpleAccent,
-      body: Container(
+      body: SizedBox(
+        height: double.maxFinite,
+        width: double.maxFinite,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/icons/ic_splash.png"),
-              SizedBox(height: 10),
-              Text("CV Maker",style: TextStyle(color: Colors.white,fontSize: 30),)
+              Lottie.asset('assets/lottiefiles/splash_.json'),
+              const SizedBox(height: 10),
+              const Text("CV Maker",style: TextStyle(color: Colors.white,fontSize: 30),)
             ],
           ),
         ),
